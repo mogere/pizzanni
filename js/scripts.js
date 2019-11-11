ba7cd463a20aa55fbff5123e39169ef734daca80
mogere <pmnyatichi@gmail.com>
2019-11-11 15:08:39 +0300

    add initial jquery code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
-    $(".large-pizza").hover(function(){
-        $("#large").fadeIn();
-        $(".choice").show();
-    }, function(){
-        $(".choice").hide();
-    });
+    // $(".large-pizza").hover(function(){
+    //     $(".large").fadeIn();
+    //     $(".choice1").show();
+    // }, function(){
+    //     $(".choice1").hide();
+    // });
 
+    // $(".medium-pizza").hover(function(){
+    //     $("#medium").fadeIn();
+    //     $(".choice2").show();
+    // }, function(){
+    //     $(".choice2").hide();
+    // });
+
+    // $(".small-pizza").hover(function(){
+    //     $("#small").fadeIn();
+    //     $(".choice3").show();
+    // }, function(){
+    //     $(".choice3").hide();
+    // });
+
+    $("#large").click(function(){
+        $("#.items").append('<tr class="item"><td> 1 Large Pizza</td><td>1,200</td></tr>')
+    });
 });
