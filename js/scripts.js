cdbe7347a8e9ca80d7daaf5a5e11e83f2bae7e72
mogere <pmnyatichi@gmail.com>
2019-11-11 15:45:53 +0300

    add append to table data

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,25 @@ $(document).ready(function(){
     // }, function(){
     //     $(".choice3").hide();
     // });
-
+    // function pizza(size, cost){
+    //     this.size=size;
+    //     this.cost=cost;
+    // }
+    var total=0;
     $("#large").click(function(){
-        $("#.items").append('<tr class="item"><td> 1 Large Pizza</td><td>1,200</td></tr>')
+        total=total+1,200;
+        $(".items").append('<tr class="item"><td> 1 Large Pizza</td><td>1,200</td></tr>')
     });
+    $("#medium").click(function(){
+        total=total+800;
+        $(".items").append('<tr class="item"><td> 1 medium Pizza</td><td>800</td></tr>')
+    });
+    $("#small").click(function(){
+        total=total+600;
+        $(".items").append('<tr class="item"><td> 1 small Pizza</td><td>600</td></tr>')
+    });
+
+    function appendTotal(){
+        $(".items").append('<tr class="item"><td> Your total is:</td><td>total</td></tr>')
+    }
 });
